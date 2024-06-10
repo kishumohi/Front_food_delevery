f6bd6ae4701105e90c916b8fcfe2bbb06a1497e9
kishan <kishankalavadiya@gmail.com>
2024-06-10 21:16:15 +0530

    till food item display

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-# Complete List
+# Complete List -- 1:24:57
     - [01] - Done - Navigation Bar
     - [02] - Done - Create Pages
     - [03] - Done - Setup React Router
--- src/pages/Home/Homeg.jsx
+++ src/pages/Home/Homeg.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header.jsx";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay.jsx";
 
 function Home() {
   const [category, setCategory] = useState("All");
@@ -10,6 +11,7 @@ function Home() {
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   );
 }
